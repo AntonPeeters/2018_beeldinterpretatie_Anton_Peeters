acf47e0c7b42d0626de6d6fb6847b27162d00a94
AntonPeeters <antonpeeters96@gmail.com>
2018-11-10 15:52:29 +0100

    Added trackbars and contours

--- sessie_2/main.cpp
+++ sessie_2/main.cpp
@@ -65,46 +65,11 @@ int main(int argc, const char** argv)
     Mat BLUE = channels[0];
     Mat GREEN = channels[1];
     Mat RED = channels[2];
-    Mat rgbsegm = Mat::zeros(imageSign1.rows, imageSign1.cols, CV_8UC1);
 
-    //rgbsegm = (RED>150) & (GREEN<255) & (BLUE<255);
     Mat RED_th;
     threshold(RED, RED_th, 150, 255,THRESH_BINARY);
-    //rgbsegm = rgbsegm*255;
     imshow("Only traffic sign", RED_th);
     waitKey(0);
-
-    /*
-    /// Using erode and dilate to suppress the noise
-    /// First getting rid of noise
-    erode(rgbsegm, rgbsegm, Mat(), Point(-1,-1),2);
-    dilate(rgbsegm, rgbsegm, Mat(), Point(-1,-1),2);
-    imshow("no noise", rgbsegm);
-    waitKey(0);
-
-    /// Then connecting blobs
-    dilate(rgbsegm, rgbsegm, Mat(), Point(-1,-1),5);
-    erode(rgbsegm, rgbsegm, Mat(), Point(-1,-1),5);
-    imshow("connecting blobs", rgbsegm);
-    waitKey(0);
-
-    /// Drawing contours on the binary image
-    canvas = img.clone();
-    vector<vector<Point>> contours;
-    findContours(rgbsegm.clone(), contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
-    vector<Point> grootst_blob = CONTOURS[0];
-    for(int i=0; i < contours.size(); i++) {
-        if(contourArea(contours[i]) > contourArea(grootste_blob)){
-            grootste_blob = contours[i];
-        }
-    }
-    vector<vector<Points>> temp;
-    temp.push_back(grootste_blob);
-    drawContours(canvas, temp, -1, Scalar(255,255,0), 3);
-    imshow("verkeersbord",canvas);
-
-    (hierna eventueel nog hulls)
-    */
 
     /// Merging the original image with the contoured image in order to get a "mask"
     Mat finaal(imageSign1.rows, imageSign1.cols, CV_8UC3);
@@ -119,29 +84,25 @@ int main(int argc, const char** argv)
     imshow("Mask", finaal);
     waitKey(0);
 
+    /// Create Windows
+    namedWindow("With trackbars", 1);
+
+    /// Create Trackbars
+    createTrackbar( "Hue min", "With trackbars", &alpha_slider, alpha_slider_max, in_trackbar );
+    createTrackbar( "Hue max", "With trackbars", &alpha_slider2, alpha_slider_max2, in_trackbar2 );
+    createTrackbar( "Saturation", "With trackbars", &alpha_slider3, alpha_slider_max3, in_trackbar3 );
+
+    /// Show some stuff
+    in_trackbar( alpha_slider, 0 );
+    in_trackbar2( alpha_slider2, 0 );
+    in_trackbar3( alpha_slider3, 0 );
+
+    while(true)
+    {
         /// Segmenting all the red pixels from loaded picture using HSV
         vector<Mat> channels2;
         Mat imageSign1_hsv;
         cvtColor(imageSign1, imageSign1_hsv, CV_BGR2HSV);
-        imshow("original", imageSign1_hsv);
-        waitKey(0);
-
-            /// Create Windows
-    namedWindow("Trackbars", 1);
-
-    /// Create Trackbars
-    char TrackbarName[50];
-    createTrackbar( TrackbarName, "Trackbars", &alpha_slider, alpha_slider_max, in_trackbar );
-    in_trackbar( alpha_slider, 0 );
-    createTrackbar( TrackbarName, "Trackbars", &alpha_slider2, alpha_slider_max2, in_trackbar2 );
-    in_trackbar2( alpha_slider2, 0 );
-    createTrackbar( TrackbarName, "Trackbars", &alpha_slider3, alpha_slider_max3, in_trackbar3 );
-    in_trackbar3( alpha_slider3, 0 );
-
-    /// Show some stuff
-
-
-
 
         split(imageSign1_hsv,channels2);
         Mat HUE = channels2[0];
@@ -154,14 +115,60 @@ int main(int argc, const char** argv)
         inRange(SAT, s1,255,s_dest);
 
         HUE = (h_dest1 | h_dest2) & s_dest;
-        imshow("Trackbars", HUE);
-        waitKey(0);
 
         /// Using erode and dilate to suppress the noise
         /// First getting rid of noise
         erode(HUE, HUE, Mat(), Point(-1,-1),2);
         dilate(HUE, HUE, Mat(), Point(-1,-1),2);
-        imshow("no noise", HUE);
-        waitKey(0);
+
+        /// Then connecting blobs
+        dilate(HUE, HUE, Mat(), Point(-1,-1),5);
+        erode(HUE, HUE, Mat(), Point(-1,-1),5);
+
+        imshow("With trackbars", HUE);
+
+        /// Wait until user press some key for 50ms
+        int iKey = waitKey(50);
+
+        ///if user press 'Enter' key
+        if (iKey == 13)
+        {
+            break;
+        }
+    }
+
+    /// Drawing contours on the traffic sign
+    vector<Mat> channels3;
+    Mat imageSign1_hsv;
+    cvtColor(imageSign1, imageSign1_hsv, CV_BGR2HSV);
+
+    split(imageSign1_hsv,channels3);
+    Mat HUE = channels3[0];
+    Mat SAT = channels3[1];
+    Mat VAL = channels3[2];
+
+    /// Using the above selected values of the user
+    Mat h_dest1, h_dest2, s_dest;
+    inRange(HUE, h1,180,h_dest1);
+    inRange(HUE, 0,h2,h_dest2);
+    inRange(SAT, s1,255,s_dest);
+
+    HUE = (h_dest1 | h_dest2) & s_dest;
+
+    Mat canvas = imageSign1.clone();
+    vector<vector<Point>> contours;
+    findContours(HUE.clone(), contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+    vector<Point> grootste_blob = contours[0];
+    for(int i=0; i < contours.size(); i++) {
+        if(contourArea(contours[i]) > contourArea(grootste_blob)){
+            grootste_blob = contours[i];
+        }
+    }
+    vector<vector<Point>> temp;
+    temp.push_back(grootste_blob);
+    drawContours(canvas, temp, -1, Scalar(255,255,0), 3);
+    imshow("verkeersbord",canvas);
+    waitKey(0);
+
     return 0;
 }
